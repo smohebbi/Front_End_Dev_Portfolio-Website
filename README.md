d9d02ba564f65e1661cf6c71908de0c4116ecc3f
smohebbi <siavash.mohebbi@gmail.com>
2022-02-01 13:00:04 +0100

    improve website: add projects, add fun fact, add contact page

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Front_End_Dev_Portfolio-Website
 
+Markdown: README.md
+
+DOM and Javascript:
+Git & GitHub:
+
+[CSS]
+Responsive Design:
+	@media only screen and (max-width: 768px)
+Font:
+	<link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital@1&display=swap" rel="stylesheet">
+	style.css: @font-face
+BreadScrum:
+Animation: 
+Flex:
+Grid:
+
+[API]
--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+let items = document.getElementsByClassName('item');
+for (const item in items) {
+    items[item].onclick = () => {
+        let image = items[item].nextElementSibling;
+        if (image.style.display == 'block') {
+            image.style.display = 'none';
+        } else {
+            image.style.display = 'block';
+        }
+    }         
+}
 
->>>>>>> 3758ab030b10d8b9c9cc3884f49fa3aeb8151855
-=======
-
->>>>>>> 3758ab030b10d8b9c9cc3884f49fa3aeb8151855
+//*****************************
+//fun fact
+const source = document.getElementById('funFact').innerHTML;
+const body = document.body;
+let numOfClicks = 0;
+body.addEventListener('click', () => {  
+    numOfClicks += 1;
+    document.getElementById('funFact').innerHTML = `Fun fact: you have clicked ${numOfClicks} times.`;
+});
